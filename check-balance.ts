4035543d64cbf17df3dee2c0cbad9cddd060be80
shtah <1c@ua.fm>
2024-08-13 01:43:06 +0400

    Create load-keypairs script and update generate-keypairs
    Added a new script, load-keypairs.ts, to load key pairs from an environment variable. Modified generate-keypairs.ts to simplify keypair generation, removing the environment variable dependency.

---
+++ check-balance.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import {
+    Connection,
+    LAMPORTS_PER_SOL,
+    PublicKey,
+    clusterApiUrl,
+} from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+console.log(`⚡️ Connected to devnet`);
+
+const publicKey = new PublicKey("4p5yuz23iphkExs8gKxcvowQUiNJZhuGEpXVspBa8iPA");
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+    `💰 The balance for the wallet at address ${publicKey} is: ${balanceInSOL}`
+);
+
